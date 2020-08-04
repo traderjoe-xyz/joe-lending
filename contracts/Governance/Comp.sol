b70d1a65c8017b642a9a3e113f625cbae36124c0
Jeremy Yang <kaiwei19@gmail.com>
2020-08-05 02:15:32 +0800

    Update Cream token contract

--- contracts/Governance/Comp.sol
+++ contracts/Governance/Comp.sol
@@ -3,16 +3,16 @@ pragma experimental ABIEncoderV2;
 
 contract Comp {
     /// @notice EIP-20 token name for this token
-    string public constant name = "Compound";
+    string public constant name = "Cream";
 
     /// @notice EIP-20 token symbol for this token
-    string public constant symbol = "COMP";
+    string public constant symbol = "CREAM";
 
     /// @notice EIP-20 token decimals for this token
     uint8 public constant decimals = 18;
 
     /// @notice Total number of tokens in circulation
-    uint public constant totalSupply = 10000000e18; // 10 million Comp
+    uint public constant totalSupply = 9000000e18; // 9 million Comp
 
     /// @notice Allowance amounts on behalf of others
     mapping (address => mapping (address => uint96)) internal allowances;
