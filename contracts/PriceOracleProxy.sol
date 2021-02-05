275efb2b389e83a6faa9a2a692618a9e66a5310e
bun919tw <bun919tw@gmail.com>
2021-02-05 19:17:13 +0800

    contracts/: support LP token price

--- contracts/PriceOracleProxy.sol
+++ contracts/PriceOracleProxy.sol
@@ -10,7 +10,7 @@ interface V1PriceOracleInterface {
     function assetPrices(address asset) external view returns (uint);
 }
 
-interface YSwapInterface {
+interface CurveSwapInterface {
     function get_virtual_price() external view returns (uint256);
 }
 
@@ -18,15 +18,125 @@ interface YVaultInterface {
     function getPricePerFullShare() external view returns (uint256);
 }
 
-interface AggregatorInterface {
-  function latestAnswer() external view returns (int256);
-  function latestTimestamp() external view returns (uint256);
-  function latestRound() external view returns (uint256);
-  function getAnswer(uint256 roundId) external view returns (int256);
-  function getTimestamp(uint256 roundId) external view returns (uint256);
+interface AggregatorV3Interface {
+    function decimals() external view returns (uint8);
+    function description() external view returns (string memory);
+    function version() external view returns (uint256);
 
-  event AnswerUpdated(int256 indexed current, uint256 indexed roundId, uint256 timestamp);
-  event NewRound(uint256 indexed roundId, address indexed startedBy);
+    // getRoundData and latestRoundData should both raise "No data present"
+    // if they do not have data to report, instead of returning unset values
+    // which could be misinterpreted as actual reported values.
+    function getRoundData(uint80 _roundId) external view returns (
+        uint80 roundId,
+        int256 answer,
+        uint256 startedAt,
+        uint256 updatedAt,
+        uint80 answeredInRound
+    );
+
+    function latestRoundData() external view returns (
+        uint80 roundId,
+        int256 answer,
+        uint256 startedAt,
+        uint256 updatedAt,
+        uint80 answeredInRound
+    );
+}
+
+// Ref: https://github.com/Uniswap/uniswap-v2-core/blob/master/contracts/interfaces/IUniswapV2Pair.sol
+interface IUniswapV2Pair {
+    event Approval(address indexed owner, address indexed spender, uint value);
+    event Transfer(address indexed from, address indexed to, uint value);
+
+    function name() external pure returns (string memory);
+
+    function symbol() external pure returns (string memory);
+
+    function decimals() external pure returns (uint8);
+
+    function totalSupply() external view returns (uint);
+
+    function balanceOf(address owner) external view returns (uint);
+
+    function allowance(address owner, address spender) external view returns (uint);
+
+    function approve(address spender, uint value) external returns (bool);
+
+    function transfer(address to, uint value) external returns (bool);
+
+    function transferFrom(
+        address from,
+        address to,
+        uint value
+    ) external returns (bool);
+
+    function DOMAIN_SEPARATOR() external view returns (bytes32);
+
+    function PERMIT_TYPEHASH() external pure returns (bytes32);
+
+    function nonces(address owner) external view returns (uint);
+
+    function permit(
+        address owner,
+        address spender,
+        uint value,
+        uint deadline,
+        uint8 v,
+        bytes32 r,
+        bytes32 s
+    ) external;
+
+    event Mint(address indexed sender, uint amount0, uint amount1);
+    event Burn(address indexed sender, uint amount0, uint amount1, address indexed to);
+    event Swap(
+        address indexed sender,
+        uint amount0In,
+        uint amount1In,
+        uint amount0Out,
+        uint amount1Out,
+        address indexed to
+    );
+    event Sync(uint112 reserve0, uint112 reserve1);
+
+    function MINIMUM_LIQUIDITY() external pure returns (uint);
+
+    function factory() external view returns (address);
+
+    function token0() external view returns (address);
+
+    function token1() external view returns (address);
+
+    function getReserves()
+        external
+        view
+        returns (
+            uint112 reserve0,
+            uint112 reserve1,
+            uint32 blockTimestampLast
+        );
+
+    function price0CumulativeLast() external view returns (uint);
+
+    function price1CumulativeLast() external view returns (uint);
+
+    function kLast() external view returns (uint);
+
+    function mint(address to) external returns (uint liquidity);
+
+    function burn(address to) external returns (uint amount0, uint amount1);
+
+    function swap(
+        uint amount0Out,
+        uint amount1Out,
+        address to,
+        bytes calldata data
+    ) external;
+
+    function skim(address to) external;
+
+    function sync() external;
+
+    function initialize(address, address) external;
 }
 
 contract PriceOracleProxy is PriceOracle, Exponential {
@@ -39,13 +149,18 @@ contract PriceOracleProxy is PriceOracle, Exponential {
     V1PriceOracleInterface public v1PriceOracle;
 
     /// @notice Chainlink Aggregators
-    mapping(address => AggregatorInterface) public aggregators;
+    mapping(address => AggregatorV3Interface) public aggregators;
+
+    /// @notice Check if the address is Uniswap or SushiSwap LP
+    mapping(address => bool) public areLPs;
 
     address public cEthAddress;
     address public cUsdcAddress;
     address public cYcrvAddress;
-    address public cYYcrvAddress;
+    address public cYusdAddress;
     address public cYethAddress;
+
+    address public constant wethAddress = 0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2;
 
     /**
      * @param admin_ The address of admin to set aggregators
@@ -53,7 +168,7 @@ contract PriceOracleProxy is PriceOracle, Exponential {
      * @param cEthAddress_ The address of cETH, which will return a constant 1e18, since all prices relative to ether
      * @param cUsdcAddress_ The address of cUSDC
      * @param cYcrvAddress_ The address of cYcrv
-     * @param cYYcrvAddress_ The address of cYYcrv
+     * @param cYusdAddress_ The address of cYusd
      * @param cYethAddress_ The address of cYeth
      */
     constructor(address admin_,
@@ -61,14 +176,14 @@ contract PriceOracleProxy is PriceOracle, Exponential {
                 address cEthAddress_,
                 address cUsdcAddress_,
                 address cYcrvAddress_,
-                address cYYcrvAddress_,
+                address cYusdAddress_,
                 address cYethAddress_) public {
         admin = admin_;
         v1PriceOracle = V1PriceOracleInterface(v1PriceOracle_);
         cEthAddress = cEthAddress_;
         cUsdcAddress = cUsdcAddress_;
         cYcrvAddress = cYcrvAddress_;
-        cYYcrvAddress = cYYcrvAddress_;
+        cYusdAddress = cYusdAddress_;
         cYethAddress = cYethAddress_;
     }
 
@@ -79,102 +194,136 @@ contract PriceOracleProxy is PriceOracle, Exponential {
      */
     function getUnderlyingPrice(CToken cToken) public view returns (uint) {
         address cTokenAddress = address(cToken);
-
         if (cTokenAddress == cEthAddress) {
             // ether always worth 1
             return 1e18;
         }
 
+        address underlying = CErc20(cTokenAddress).underlying();
         if (cTokenAddress == cYethAddress) {
             uint yVaultPrice = YVaultInterface(0xe1237aA7f535b0CC33Fd973D66cBf830354D16c7).getPricePerFullShare();
             if (yVaultPrice == 0) {
-                return getPriceFromV1(cTokenAddress);
+                return getPriceFromV1(underlying);
             }
             return yVaultPrice;
         }
 
-        if (cTokenAddress == cYcrvAddress || cTokenAddress == cYYcrvAddress) {
-            MathError mathErr;
-            Exp memory ethUsdPrice;
-            Exp memory yCrvPrice;
-
+        if (cTokenAddress == cYcrvAddress || cTokenAddress == cYusdAddress) {
             // ETH/USDC (treat USDC as USD)
-            ethUsdPrice.mantissa = getUnderlyingPrice(CToken(cUsdcAddress)) / 1e12;
-            if (ethUsdPrice.mantissa == 0) {
-              return 0;
+            uint ethUsdPrice = getUnderlyingPrice(CToken(cUsdcAddress)) / 1e12;
+            if (ethUsdPrice == 0) {
+                return 0;
             }
 
             // YCRV/USD
-            uint virtualPrice = YSwapInterface(0x45F783CCE6B7FF23B2ab2D70e416cdb7D6055f51).get_virtual_price();
+            uint virtualPrice = CurveSwapInterface(0x45F783CCE6B7FF23B2ab2D70e416cdb7D6055f51).get_virtual_price();
 
             // YCRV/ETH = USD/ETH * YCRV/USD
-            (mathErr, yCrvPrice) = mulExp(ethUsdPrice, Exp({mantissa: virtualPrice}));
-            if (mathErr != MathError.NO_ERROR) {
-                // Fallback to v1 PriceOracle
-                return getPriceFromV1(cTokenAddress);
-            }
+            uint yCrvPrice = mul_(ethUsdPrice, Exp({mantissa: virtualPrice}));
 
-            if (cTokenAddress == cYYcrvAddress) {
+            if (cTokenAddress == cYusdAddress) {
+                // YUSD/YCRV
                 uint yVaultPrice = YVaultInterface(0x5dbcF33D8c2E976c6b560249878e6F1491Bca25c).getPricePerFullShare();
-                Exp memory yyCrvPrice;
-                (mathErr, yyCrvPrice) = mulExp(yCrvPrice, Exp({mantissa: yVaultPrice}));
-                if (mathErr != MathError.NO_ERROR) {
-                    // Fallback to v1 PriceOracle
-                    return getPriceFromV1(cTokenAddress);
-                }
-                return yyCrvPrice.mantissa;
+
+                // YUSD/ETH = YCRV/ETH * YUSD/YCRV
+                uint yUSDPrice = mul_(yCrvPrice, Exp({mantissa: yVaultPrice}));
+                return yUSDPrice;
             }
-            return yCrvPrice.mantissa;
+            return yCrvPrice;
         }
 
-        AggregatorInterface aggregator = aggregators[cTokenAddress];
+        if (areLPs[underlying]) {
+            return getLPFairPrice(underlying);
+        }
+
+        return getTokenPrice(underlying);
+    }
+
+    /*** Internal fucntions ***/
+
+    /**
+     * @notice Get the price of a specific token. Return 1e18 is it's WETH.
+     * @param token The token to get the price of
+     * @return The price
+     */
+    function getTokenPrice(address token) internal view returns (uint) {
+        if (token == wethAddress) {
+            // weth always worth 1
+            return 1e18;
+        }
+
+        AggregatorV3Interface aggregator = aggregators[token];
         if (address(aggregator) != address(0)) {
-            MathError mathErr;
-            Exp memory price;
-            (mathErr, price) = getPriceFromChainlink(aggregator);
-            if (mathErr != MathError.NO_ERROR) {
-                // Fallback to v1 PriceOracle
-                return getPriceFromV1(cTokenAddress);
-            }
-
-            if (price.mantissa == 0) {
-                return getPriceFromV1(cTokenAddress);
-            }
-
-            uint underlyingDecimals;
-            underlyingDecimals = EIP20Interface(CErc20(cTokenAddress).underlying()).decimals();
-            (mathErr, price) = mulScalar(price, 10**(18 - underlyingDecimals));
-            if (mathErr != MathError.NO_ERROR ) {
-                // Fallback to v1 PriceOracle
-                return getPriceFromV1(cTokenAddress);
-            }
-
-            return price.mantissa;
+            uint price = getPriceFromChainlink(aggregator);
+            uint underlyingDecimals = EIP20Interface(token).decimals();
+            return mul_(price, 10**(18 - underlyingDecimals));
         }
-
-        return getPriceFromV1(cTokenAddress);
+        return getPriceFromV1(token);
     }
 
-    function getPriceFromChainlink(AggregatorInterface aggregator) internal view returns (MathError, Exp memory) {
-        int256 chainLinkPrice = aggregator.latestAnswer();
-        if (chainLinkPrice <= 0) {
-            return (MathError.INTEGER_OVERFLOW, Exp({mantissa: 0}));
-        }
-        return (MathError.NO_ERROR, Exp({mantissa: uint(chainLinkPrice)}));
+    /**
+     * @notice Get price from ChainLink
+     * @param aggregator The ChainLink aggregator to get the price of
+     * @return The price
+     */
+    function getPriceFromChainlink(AggregatorV3Interface aggregator) internal view returns (uint) {
+        ( , int price, , , ) = aggregator.latestRoundData();
+        require(price > 0, "invalid price");
+
+        // Extend the decimals to 1e18.
+        return mul_(uint(price), 10**(18 - uint(aggregator.decimals())));
     }
 
-    function getPriceFromV1(address cTokenAddress) internal view returns (uint) {
-        address underlying = CErc20(cTokenAddress).underlying();
-        return v1PriceOracle.assetPrices(underlying);
+    /**
+     * @notice Get the fair price of a LP. We use the mechanism from Alpha Finance.
+     *         Ref: https://blog.alphafinance.io/fair-lp-token-pricing/
+     * @param pair The pair of AMM (Uniswap or SushiSwap)
+     * @return The price
+     */
+    function getLPFairPrice(address pair) internal view returns (uint) {
+        address token0 = IUniswapV2Pair(pair).token0();
+        address token1 = IUniswapV2Pair(pair).token1();
+        uint totalSupply = IUniswapV2Pair(pair).totalSupply();
+        (uint r0, uint r1, ) = IUniswapV2Pair(pair).getReserves();
+        uint sqrtR = sqrt(mul_(r0, r1));
+        uint p0 = getTokenPrice(token0);
+        uint p1 = getTokenPrice(token1);
+        uint sqrtP = sqrt(mul_(p0, p1));
+        return div_(mul_(2, mul_(sqrtR, sqrtP)), totalSupply);
     }
 
-    event AggregatorUpdated(address cTokenAddress, address source);
+    /**
+     * @notice Get price from v1 price oracle
+     * @param token The token to get the price of
+     * @return The price
+     */
+    function getPriceFromV1(address token) internal view returns (uint) {
+        return v1PriceOracle.assetPrices(token);
+    }
 
-    function _setAggregators(address[] calldata cTokenAddresses, address[] calldata sources) external {
+    event AggregatorUpdated(address tokenAddress, address source);
+    event IsLPUpdated(address tokenAddress, bool isLP);
+
+    function _setAggregators(address[] calldata tokenAddresses, address[] calldata sources) external {
         require(msg.sender == admin, "only the admin may set the aggregators");
-        for (uint i = 0; i < cTokenAddresses.length; i++) {
-            aggregators[cTokenAddresses[i]] = AggregatorInterface(sources[i]);
-            emit AggregatorUpdated(cTokenAddresses[i], sources[i]);
+        require(tokenAddresses.length == sources.length, "mismatched data");
+        for (uint i = 0; i < tokenAddresses.length; i++) {
+            aggregators[tokenAddresses[i]] = AggregatorV3Interface(sources[i]);
+            emit AggregatorUpdated(tokenAddresses[i], sources[i]);
         }
+    }
+
+    function _setLPs(address[] calldata tokenAddresses, bool[] calldata isLP) external {
+        require(msg.sender == admin, "only the admin may set LPs");
+        require(tokenAddresses.length == isLP.length, "mismatched data");
+        for (uint i = 0; i < tokenAddresses.length; i++) {
+            areLPs[tokenAddresses[i]] = isLP[i];
+            emit IsLPUpdated(tokenAddresses[i], isLP[i]);
+        }
+    }
+
+    function _setAdmin(address _admin) external {
+        require(msg.sender == admin, "only the admin may set new admin");
+        admin = _admin;
     }
 }
