5ca45da3621573d79e45bfe1e0878337b93f47d3
cryptofish7 <yocryptofish7@gmail.com>
2021-08-30 03:41:46 -0400

    Change CEth deploy to use CWrappedNativeDelegator/Delegate

--- deploy/CEth.js
+++ deploy/CEth.js
@@ -8,32 +8,51 @@ module.exports = async function ({ getNamedAccounts, deployments }) {
 
   const interestRateModel = await ethers.getContract("TripleSlopeRateModel");
 
-  await deploy("CEther", {
+  await deploy("CWrappedNativeDelegate", {
+    from: deployer,
+    log: true,
+    deterministicDeployment: false,
+  });
+  const cEthDelegate = await ethers.getContract("CWrappedNativeDelegate");
+
+  const deployment = await deploy("CWrappedNativeDelegator", {
     from: deployer,
     args: [
+      "0xc778417e063141139fce010982780140aa0cd5ab", // WETH on Rinkeby
       comptroller.address,
       interestRateModel.address,
       ethers.utils.parseUnits("2", 26).toString(),
-      "Compound Ether Token",
-      "cEther",
-      "8",
+      "Wrapped Ether",
+      "WETH",
+      18,
       deployer,
+      cEthDelegate.address,
+      "0x",
     ],
     log: true,
     deterministicDeployment: false,
-    none: 2,
   });
-  const cEth = await ethers.getContract("CEther");
+  await deployment.receipt;
+  const cEthDelegator = await ethers.getContract("CUsdcDelegator");
 
   console.log("Supporting cETH market...");
-  await comptroller._supportMarket(cEth.address, 2);
+  await comptroller._supportMarket(cEthDelegator.address, 2, {
+    gasLimit: 4000000,
+  });
 
   const collateralFactor = "0.75";
   console.log("Setting collateral factor ", collateralFactor);
   await comptroller._setCollateralFactor(
-    cEth.address,
-    ethers.utils.parseEther(collateralFactor)
+    cEthDelegator.address,
+    ethers.utils.parseEther(collateralFactor),
+    {
+      gasLimit: 4000000,
+    }
   );
+
+  const reserveFactor = "0.20";
+  console.log("Setting reserve factor ", reserveFactor);
+  await cEthDelegator._setReserveFactor(ethers.utils.parseEther(reserveFactor));
 };
 
 module.exports.tags = ["cETH"];
