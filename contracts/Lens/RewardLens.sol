8b72034dcdb7145599e613c9a7a32ea4c820f411
intro0000 <hadionoc@gmail.com>
2022-01-10 22:28:39 -0500

    use a struct in mapping

--- contracts/Lens/RewardLens.sol
+++ contracts/Lens/RewardLens.sol
@@ -2,30 +2,25 @@
 
 pragma solidity ^0.5.16;
 
-import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/ownership/Ownable.sol";
 
 /**
  * @notice This is a helper for fetching and maintaing supply/borrow rewards for lending markets.
  */
 contract RewardLens is Ownable {
-    mapping(address => uint256) public supplyRewardsJoePerSec;
-    mapping(address => uint256) public borrowRewardsJoePerSec;
-    mapping(address => uint256) public supplyRewardsAvaxPerSec;
-    mapping(address => uint256) public borrowRewardsAvaxPerSec;
-
-    function setSupplyRewardsJoePerSec(address market, uint256 amount) external onlyOwner {
-        supplyRewardsJoePerSec[market] = amount;
+    struct MarketRewards { 
+        uint256 supplyRewardsJoePerSec; 
+        uint256 borrowRewardsJoePerSec; 
+        uint256 supplyRewardsAvaxPerSec; 
+        uint256 borrowRewardsAvaxPerSec;
     }
 
-    function setBorrowsRewardsJoePerSec(address market, uint256 amount) external onlyOwner {
-        borrowRewardsJoePerSec[market] = amount;
-    }
+    mapping(address => MarketRewards) public allMarketRewards;
 
-    function setSupplyRewardsAvaxPerSec(address market, uint256 amount) external onlyOwner {
-        supplyRewardsAvaxPerSec[market] = amount;
-    }
-
-    function setBorrowsRewardsAvaxPerSec(address market, uint256 amount) external onlyOwner {
-        borrowRewardsAvaxPerSec[market] = amount;
+    function setMarketRewards(address market, uint256 supplyRewardsJoePerSec, uint256 borrowRewardsJoePerSec, uint256 supplyRewardsAvaxPerSec, uint256 borrowRewardsAvaxPerSec) external onlyOwner {
+        allMarketRewards[market].supplyRewardsJoePerSec = supplyRewardsJoePerSec;
+        allMarketRewards[market].borrowRewardsJoePerSec = borrowRewardsJoePerSec;
+        allMarketRewards[market].supplyRewardsAvaxPerSec = supplyRewardsAvaxPerSec;
+        allMarketRewards[market].borrowRewardsAvaxPerSec = borrowRewardsAvaxPerSec;
     }
 }
