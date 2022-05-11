4b49802a911a3b9dcc0978666c8aac1d9eb960aa
Louis Myself <louismemyself@gmail.com>
2022-05-11 15:54:35 +0200

    Adding multisig

--- deploy/RewardDistributorV2.js
+++ deploy/RewardDistributorV2.js
@@ -2,10 +2,23 @@ module.exports = async function ({ getNamedAccounts, deployments }) {
   const { deploy } = deployments;
   const { deployer } = await getNamedAccounts();
 
-  await deploy("RewardDistributorV2", {
+  let proxyOwner;
+
+  const chainId = await getChainId();
+  if (chainId == 4) {
+    // rinkeby contract addresses
+    proxyOwner = deployer.address;
+  } else if (chainId == 43114 || chainId == 31337) {
+    // multisig
+    proxyOwner = "0x3876183b75916e20d2ADAB202D1A3F9e9bf320ad";
+  }
+
+  const rewardDistributorV2 = await deploy("RewardDistributorV2", {
     from: deployer,
     proxy: {
+      owner: proxyOwner,
       proxyContract: "OpenZeppelinTransparentProxy",
+      viaAdminContract: "DefaultProxyAdmin",
       execute: {
         init: {
           methodName: "initialize",
