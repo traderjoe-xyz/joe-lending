2a7529e249592800a0a30918d666e34ab69183e7
0x0Louis <107303182+0x0Louis@users.noreply.github.com>
2022-08-20 17:09:04 +0200

    Fuji deployment (#30)
    * fuji deploy files
    
    * deploy script for MS

--- deploy/JBtc.js
+++ deploy/JBtc.js
@@ -6,6 +6,10 @@ const BTC_PRICE_FEED = new Map();
 BTC_PRICE_FEED.set("43113", "0x31CF013A08c6Ac228C94551d535d5BAfE19c602a");
 BTC_PRICE_FEED.set("43114", "0x2779D32d5166BAaa2B2b658333bA7e6Ec0C65743");
 
+const MULTI_SIG = new Map();
+MULTI_SIG.set("43113", "0xdB40a7b71642FE24CC546bdF4749Aa3c0B042f78");
+MULTI_SIG.set("43114", "0x3876183b75916e20d2ADAB202D1A3F9e9bf320ad");
+
 module.exports = async function ({
   getChainId,
   getNamedAccounts,
@@ -23,17 +27,17 @@ module.exports = async function ({
 
   const jBtcDelegate = await ethers.getContract("JCollateralCapErc20Delegate");
 
-  const deployment = await deploy("JWbtcDelegator", {
+  const deployment = await deploy("JBtcDelegator", {
     from: deployer,
     args: [
-      WBTC.get(chainId),
+      BTC.get(chainId),
       joetroller.address,
       interestRateModel.address,
       ethers.utils.parseUnits("2", 16).toString(),
       "Banker Joe Bitcoin",
       "jBTC",
       8,
-      deployer,
+      MULTI_SIG.get(chainId),
       jBtcDelegate.address,
       "0x",
     ],
@@ -44,43 +48,81 @@ module.exports = async function ({
   await deployment.receipt;
   const jBtcDelegator = await ethers.getContract("JBtcDelegator");
 
-  console.log("Supporting jWBTC market...");
-  await joetroller._supportMarket(jBtcDelegator.address, 1, {
-    gasLimit: 2000000,
-  });
+  console.log("Supporting jBTC market...");
+  // await joetroller._supportMarket(jBtcDelegator.address, 1, {
+  //   gasLimit: 2000000,
+  // });
+
+  console.log(
+    joetroller.address,
+    joetroller.interface.encodeFunctionData("_supportMarket", [
+      jBtcDelegator.address,
+      1,
+    ])
+  );
 
   const priceOracle = await ethers.getContract("PriceOracleProxyUSD");
   console.log("Setting price feed source for jBTC");
-  await priceOracle._setAggregators(
-    [jBtcDelegator.address],
-    [BTC_PRICE_FEED.get(chainId)]
+  // await priceOracle._setAggregators(
+  //   [jBtcDelegator.address],
+  //   [BTC_PRICE_FEED.get(chainId)]
+  // );
+
+  console.log(
+    priceOracle.address,
+    priceOracle.interface.encodeFunctionData("_setAggregators", [
+      [jBtcDelegator.address],
+      [BTC_PRICE_FEED.get(chainId)],
+    ])
   );
 
   const collateralFactor = "0.75";
   console.log("Setting collateral factor ", collateralFactor);
-  await joetroller._setCollateralFactor(
-    jBtcDelegator.address,
-    ethers.utils.parseEther(collateralFactor)
+  // await joetroller._setCollateralFactor(
+  //   jBtcDelegator.address,
+  //   ethers.utils.parseEther(collateralFactor)
+  // );
+
+  console.log(
+    joetroller.address,
+    joetroller.interface.encodeFunctionData("_setCollateralFactor", [
+      jBtcDelegator.address,
+      ethers.utils.parseEther(collateralFactor),
+    ])
   );
 
   const reserveFactor = "0.20";
   console.log("Setting reserve factor ", reserveFactor);
-  await jBtcDelegator._setReserveFactor(ethers.utils.parseEther(reserveFactor));
+  // await jBtcDelegator._setReserveFactor(ethers.utils.parseEther(reserveFactor));
+
+  console.log(
+    jBtcDelegator.address,
+    jBtcDelegate.interface.encodeFunctionData("_setReserveFactor", [
+      ethers.utils.parseEther(reserveFactor),
+    ])
+  );
 
   const protocolSeizeShare = "0.016";
   console.log("Setting protocol seize share ", protocolSeizeShare);
-  await jBtcDelegator._setProtocolSeizeShare(
-    ethers.utils.parseEther(protocolSeizeShare)
+  // await jBtcDelegator._setProtocolSeizeShare(
+  //   ethers.utils.parseEther(protocolSeizeShare)
+  // );
+
+  console.log(
+    jBtcDelegator.address,
+    jBtcDelegate.interface.encodeFunctionData("_setProtocolSeizeShare", [
+      ethers.utils.parseEther(protocolSeizeShare),
+    ])
   );
 };
 
 module.exports.tags = ["jBTC"];
-module.exports.dependencies = [
-  "Joetroller",
-  "TripleSlopeRateModel",
-  "PriceOracle",
-  "JCollateralCapErc20Delegate",
-];
+// module.exports.dependencies = [
+//   "Joetroller",
+//   "TripleSlopeRateModel",
+//   "PriceOracle",
+//   "JCollateralCapErc20Delegate",
+// ];
 module.exports.skip = async () => {
   const chainId = await getChainId();
   if (!BTC.has(chainId)) {
